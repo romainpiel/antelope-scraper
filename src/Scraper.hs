a44b0f258e9534cea80d6110279fc05d3db0d386
Romain Piel <romain.piel@gmail.com>
2016-06-09 18:05:53 +0100

    starting parsing event + corrected a few convention hoops

--- src/Scraper.hs
+++ src/Scraper.hs
@@ -5,13 +5,32 @@ import Text.XML.Light
 main :: IO ()
 main = putStrLn "hello"
 
-findEventsHtml :: String -> IO [String]
+findEventsHtml :: String -> IO [Event]
 findEventsHtml xmlFile = do
   case parseXMLDoc xmlFile of
     Nothing   -> error "Failed to parse xml"
     Just doc  -> 
-      let events            = findElements (unqual "event") $ doc
-          cdata_containers  = map elContent events
-          cdatas            = concatMap onlyText cdata_containers
-          cdata_contents    = map cdData cdatas
-      in return cdata_contents
+      let xmlEvents        = findElements (unqual "event") $ doc
+          cdataContainers  = map elContent xmlEvents
+          cdatas            = concatMap onlyText cdataContainers
+          cdataContents    = map cdData cdatas
+          events            = map htmlContentToEvent cdataContents
+      in return events
+
+htmlContentToEvent :: String -> Event
+htmlContentToEvent html =
+  case parseXMLDoc html of
+    Nothing   -> error "Failed to parse xml"
+    Just doc  -> htmlToEvent doc
+      
+htmlToEvent :: Element -> Event
+htmlToEvent html =
+  let title = "a"
+  in Event title "" "" ""
+      
+data Event = Event {
+  name :: String, 
+  distance :: String, 
+  location :: String, 
+  url :: String
+} deriving (Show)
